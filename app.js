9fb9e1443074d777c8dbe0bdaf47d4fe104d0ea4
cyrilkong <cyril.kong@gmail.com>
2012-06-07 18:18:55 +0800

    app testing socket.io

--- app.js
+++ app.js
@@ -5,7 +5,8 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , http = require('http');
+  , http = require('http')
+  , io = require('socket.io');
 
 var app = express();
 
